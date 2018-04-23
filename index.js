41cee0f66700e2c7eed52480f493ada6edb752d2
Eric Kim <hyeokjungkim@gmail.com>
2018-04-23 17:33:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  drivers.forEach(
+    function(element){
+      console.log(element.name)
+    }
+  )
+}
+
+function logDriversByHometown(drivers, hometown){
+  arr = drivers.filter(
+    function(element){
+      return element.hometown === hometown
+    }
+  )
+  arr.forEach(
+    function(element){
+      console.log(element.name)
+    }
+  )
+}
+
+function driversByRevenue(drivers){
+  return drivers.slice().sort(function(a, b){
+      return a.revenue - b.revenue
+    }
+  )
+}
+
+function driversByName(drivers) {
+  return drivers.slice().sort(function(a,b){
+      return a.name.localeCompare(b.name)
+    }
+  )
+}
+
+function totalRevenue(drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers) / drivers.length
+}
